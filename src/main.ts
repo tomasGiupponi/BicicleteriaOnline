59e22ab3a717e700f72de692154f3be1ddefd0b8
tomasGiupponi <giupponitomas101@gmail.com>
2024-09-25 10:39:43 -0300

    Uploading changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
+
+  const config = new DocumentBuilder()
+  .setTitle('History of Data Base Requests')
+  .setDescription('-')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
   await app.listen(4000); //backend en puerto 4000
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,32 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Products')
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({summary: 'Posted a new product'})
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiResponse({status:200, description:'List of all products'})
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({status:200, description:'Found a product'})
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({summary:'Modified a product'})
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOperation({summary:'Deleted a product'})
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
